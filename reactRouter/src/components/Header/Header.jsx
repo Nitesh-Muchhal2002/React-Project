7c1943e0364943c03270ea6f40fb283ef51f1423
nitesh <nitsmuchhal@gmail.com>
2024-08-08 16:52:10 +0530

    new project

--- reactRouter/src/components/Header/Header.jsx
+++ reactRouter/src/components/Header/Header.jsx
@@ -4,11 +4,11 @@ import { Link, NavLink } from 'react-router-dom'
 export default function Header() {
   return (
     <header className="shadow sticky z-50 top-0">
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+      <nav className="bg-white border-black-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
-              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+              src="https://img.freepik.com/premium-photo/gold-symbol-with-black-gold-symbol-it_931553-271038.jpg?size=626&ext=jpg&ga=GA1.1.1248301084.1721391804&semt=ais_hybrid"
               className="mr-3 h-12"
               alt="Logo"
             />
@@ -22,9 +22,9 @@ export default function Header() {
             </Link>
             <Link
               to="#"
-              className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className="text-white bg-red-700 hover:bg-green-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
-              Get started
+              SignUp
             </Link>
           </div>
           <div
